9a64ce4bab284d59e552e250e8d56e588b6ea5cb
Aleksei Stukalov <stukalov.nz@gmail.com>
2025-01-03 22:55:28 +1300

    feat: add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+// import path from 'path'
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  resolve: {
+    alias: {
+      '@game': '/src/game',
+      '@layers': '/src/layers',
+      '@styles': '/src/styles',
+    },
+  },
 })
